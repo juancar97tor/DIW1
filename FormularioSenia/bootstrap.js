0ca06e601b252614b00433bdf6e9a73b2f0d0b88
juancar97tor <43141646+juancar97tor@users.noreply.github.com>
2018-10-29 15:48:04 +0100

    Subidotodo

---
+++ FormularioSenia/bootstrap.js
@@ -0,0 +1,18 @@
+function onSubmit( form ){
+    
+    var data = JSON.stringify( $(form).serializeArray() ); //  <-----------
+    console.log( data );
+    return false; //don't submit
+  }
+  var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
